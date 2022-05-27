654fd098827031dd39303bece9e5a225b7e913f9
Nathalya Amorim <nathalyamorim87@gmail.com>
2022-05-27 18:23:18 -0400

    first part of the code

--- script/main.js
+++ script/main.js
@@ -2,6 +2,36 @@
 
 	// define the element you want the user to interact with
 	// let theBadge = document.querySelector("#badge")
+   //=============================================== 
+
 	// set up the puzzle pieces and boards
+
+    //  Collect the buttons at the bottom of the page
+	let theThumbnails = document.querySelectorAll("#buttonHolder img"),
+	gameBoard = document.querySelector(".puzzle-board");
+
+	/* theThumbnails collects all of the image elemets into an array-like container that looks like this:
+	[
+			<img src="images/buttonZero.jpg" alt="thumbnail">
+			<img src="images/buttonOne.jpg" alt="thumbnail">
+	    	<img src="images/buttonTwo.jpg" alt="thumbnail">
+			<img src="images/buttonThree.jpg" alt="thumbnail">
+	]
+	*/
+
+	function changeBgImg() {
+		// The "this" keyword refers to the element that triggers this function ( the nav button we click with the custom data attribute of bgref)
+		// debugger;
+		gameBoard.style.backgroundImage = `url(images/backGround${this.dataset.bgref}.jpg)`
+	}
+
+	// add event handling here 
+	theThumbnails.forEach(thumb => thumb.addEventListener("click", changeBgImg));
+
+	// The name "thumbs, theThumbnails, changeBgImg" are created by me 
+
+
+
+
 	
 })();
